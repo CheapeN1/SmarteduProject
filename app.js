a3e5026245a637fc85913c2cefea934c536ccb22
CheapeN1 <134295484+CheapeN1@users.noreply.github.com>
2024-09-24 02:53:28 +0300

    Stage 3

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const pageRoute = require('./routes/pageRoute');
 
 const app = express();
 
@@ -9,17 +10,10 @@ app.set("view engine", "ejs");
 //Middlewares
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  res.status(200).render('index', {
-      page_name: "index"
-  });
-});
+//Routes
+app.use('/', pageRoute);
 
-app.get('/about', (req, res) => {
-  res.status(200).render('about', {
-      page_name: "about"
-  });
-});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`App started on port ${port}`);
