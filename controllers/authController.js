568cefa9bba8be0a0ec5b4abb81a63b445bbde13
CheapeN1 <134295484+CheapeN1@users.noreply.github.com>
2024-09-30 14:24:44 +0300

    Stage 9

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,5 @@
 const User = require('../models/User');
+const bcrypt = require('bcrypt');
 exports.createUser = async (req, res) => {
   try {
     const user = await User.create(req.body);
@@ -13,3 +14,55 @@ exports.createUser = async (req, res) => {
     });
   }
 };
+
+exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // Kullanıcıyı e-posta ile bul
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).send('Invalid email or password'); // Eğer kullanıcı bulunamazsa hata döndür
+    }
+
+    // Şifre karşılaştırma
+    const isMatch = await bcrypt.compare(password, user.password);
+    
+    if (!isMatch) {
+      return res.status(401).send('Invalid email or password'); // Eğer şifre eşleşmezse hata döndür
+    }
+
+    // Şifre eşleşirse
+    res.status(200).send('YOU ARE LOGGED IN');
+
+  } catch (error) {
+    // Hata yakalama
+    res.status(400).json({
+      status: 'fail',
+      message: 'Something went wrong',
+      error,
+    });
+  }
+};
+
+/* exports.loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    await User.findOne({ email }, (err, user) => {
+      if (user) {
+       await bcrypt.compare(password, user.password, (err, same) => {
+          if (same) {
+            // USER SESSION
+            res.status(200).send('YOU ARE LOGGED IN');
+          }
+        });
+      }
+    });
+  } catch (error) {
+    res.status(400).json({
+      status: 'fail',
+      error,
+    });
+  }
+}; */
